5018eb0a94fd1c354578c765cb48cdb105f3cf30
Divya Pawar <divyapawar2033@gmail.com>
2025-01-09 01:15:04 +0530

    updated button

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,8 +4,8 @@
             <h1 class="text-[25px] md:text-[35px] lg:text-[45px] font-bold mb-4">Unleashing the Power
                 of <span class="font-bold text-[#01afeb]">Blockchain</span></h1>
             <h1 class="text-md md:text-lg lg:text-xl mt-2 mb-4">Transforming industries with secure, decentralized, and transparent technology.</h1>
-            <button class="px-4 py-3 rounded-full bg-[#01afeb] cursor-pointer my-4 font-bold btn-animation">Get Started with Blockchain </button>
-            <button class="px-4 py-3 rounded-full cursor-pointer lg:mt-4  mb-4 border border-blue-300 md:mx-2 lg:mx-4 font-bold btn-animation">Discover How it Works</button>
+            <button class="px-4 py-3 rounded-full bg-[#01afeb] cursor-pointer my-4 font-bold btn-animation mr-2">Get Started with Blockchain </button>
+            <button class="px-4 py-3 rounded-full cursor-pointer lg:mt-4  mb-4 border border-blue-300 lg:mx-4 font-bold btn-animation">Discover How it Works</button>
         </div>
 
         <div class="w-[80%] md:w-[48%] ">
